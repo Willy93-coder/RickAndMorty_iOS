8ae0c7c6a4e8a4995c30dbaff0a1fd958c4046c9
Willy <guillecg13@gmail.com>
2024-10-16 15:24:21 +0200

    Initial Commit

---
+++ FavAppDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FavAppDemo
+//
+//  Created by Willy on 16/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FavAppDemo/FavAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavAppDemoApp.swift
+//  FavAppDemo
+//
+//  Created by Willy on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FavAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
