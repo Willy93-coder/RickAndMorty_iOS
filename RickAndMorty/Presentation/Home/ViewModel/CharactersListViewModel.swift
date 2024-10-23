acdd0759f3fb332f3ea826b2ba4e40d03984a500
Willy <guillecg13@gmail.com>
2024-10-23 11:15:45 +0200

    Modified: getCharactersUseCase for getAllCharactersUseCase

--- RickAndMorty/Presentation/Home/ViewModel/CharactersListViewModel.swift
+++ RickAndMorty/Presentation/Home/ViewModel/CharactersListViewModel.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 class CharactersListViewModel: ObservableObject {
-    var getCharactersUseCase: GetAllCharacterProtocol
+    var getAllCharactersUseCase: GetAllCharactersProtocol
     
     @Published var characters: [Character] = []
     
-    init(getCharactersUseCase: GetAllCharacterProtocol = GetAllCharactersUseCase(repository: CharacterRepositoryImpl(dataSource: CharacterDataSourceImpl()))) {
-        self.getCharactersUseCase = getCharactersUseCase
+    init(getCharactersUseCase: GetAllCharactersProtocol = GetAllCharactersUseCase(repository: CharacterRepositoryImpl(dataSource: CharacterDataSourceImpl()))) {
+        self.getAllCharactersUseCase = getCharactersUseCase
     }
     
     @MainActor func getCharacters() async {
-        let result = await getCharactersUseCase.fetchAllCharacters()
+        let result = await getAllCharactersUseCase.fetchAllCharacters()
         switch result {
         case .success(let characters):
             self.characters = characters
