9cacbe0109030a098fc2fb7280ac8041b1ca02bd
Guillermo Cifre <guillecg13@gmail.com>
2024-11-12 23:06:07 +0100

    Added: getAll, insert and remove use case

--- RickAndMorty/Presentation/CharactersListView/ViewModel/CharactersListViewModel.swift
+++ RickAndMorty/Presentation/CharactersListView/ViewModel/CharactersListViewModel.swift
@@ -8,14 +8,31 @@
 import Foundation
 
 class CharactersListViewModel: ObservableObject {
-    private var getAllCharactersUseCase: GetAllCharactersUseCaseProtocol
+    private var getAllCharactersUseCase: GetAllCharactersFromAPIUseCaseProtocol
+    private var getAllCharactersIdFromLocalDatabaseUseCase: GetAllFavCharactersIdFromLocalDatabaseUseCaseProtocol
+    private var insertFavCharacterIdIntoLocalDatabaseUseCase: InsertFavCharacterIdIntoLocalDatabaseUseCaseProtocol
+    private var removeFavCharacterFromLocalDatabaseUseCase: RemoveFavCharacterFromLocalDatabaseUseCaseProtocol
     
     @Published var characters: [Character] = []
     @Published var favCharacters: [Character] = []
-    var likesManager: FavouriteManager = FavouriteManager()
+    private var favCharactersId: [Int] = []
     
-    init(getCharactersUseCase: GetAllCharactersUseCaseProtocol = GetAllCharactersUseCase(repository: CharacterRepositoryImpl(dataSource: NetworkCharacterDataSourceImpl()))) {
+    init(getCharactersUseCase: GetAllCharactersFromAPIUseCaseProtocol = GetAllCharactersFromAPIUseCase(repository: CharacterRepositoryImpl(networkDataSource: NetworkCharacterDataSourceImpl(), localDataSource: LocalCharacterDataSourceImp.shared)),
+         getAllCharactersIdFromLocalDatabaseUseCase: GetAllFavCharactersIdFromLocalDatabaseUseCaseProtocol = GetAllFavCharactersIdFromLocalDatabaseUseCase(repository: CharacterRepositoryImpl(networkDataSource: NetworkCharacterDataSourceImpl(), localDataSource: LocalCharacterDataSourceImp.shared)),
+         insertFavCharacterIdIntoLocalDatabaseUseCase: InsertFavCharacterIdIntoLocalDatabaseUseCaseProtocol = InsertFavCharacterIdIntoLocalDatabaseUseCase(repository: CharacterRepositoryImpl(networkDataSource: NetworkCharacterDataSourceImpl(), localDataSource: LocalCharacterDataSourceImp.shared)),
+         removeFavCharacterFromLocalDatabaseUseCase: RemoveFavCharacterFromLocalDatabaseUseCaseProtocol = RemoveFavCharacterFromLocalDatabaseUseCase(repository: CharacterRepositoryImpl(networkDataSource: NetworkCharacterDataSourceImpl(), localDataSource: LocalCharacterDataSourceImp.shared))
+    ) {
         self.getAllCharactersUseCase = getCharactersUseCase
+        self.getAllCharactersIdFromLocalDatabaseUseCase = getAllCharactersIdFromLocalDatabaseUseCase
+        self.insertFavCharacterIdIntoLocalDatabaseUseCase = insertFavCharacterIdIntoLocalDatabaseUseCase
+        self.removeFavCharacterFromLocalDatabaseUseCase = removeFavCharacterFromLocalDatabaseUseCase
+        fectchAllFavCharactersFromLocalDatabase()
+        Task {
+            await getCharactersList()
+            await MainActor.run {
+                populateFavCharacters()
+            }
+        }
     }
     
     func getCharactersList() async {
@@ -26,10 +43,14 @@ class CharactersListViewModel: ObservableObject {
     }
     
     func updateFavCharactersList(_ character: Character) {
-        if !likesManager.isFavourite(id: character.id), !isFavourite(character.id) {
+        if !isFavourite(character.id) {
             addToFavList(character)
+            insertFavCharacterIdIntoLocalDatabase(character.id)
+            fectchAllFavCharactersFromLocalDatabase()
         } else {
             removeFromFavList(character)
+            removeFavCharacterIsFromLocalDatabase(character.id)
+            fectchAllFavCharactersFromLocalDatabase()
         }
     }
     
@@ -41,7 +62,29 @@ class CharactersListViewModel: ObservableObject {
         favCharacters.removeAll(where: { $0.id == character.id })
     }
     
-    private func isFavourite(_ id: Int) -> Bool {
-        favCharacters.contains { $0.id == id }
+    private func fectchAllFavCharactersFromLocalDatabase() {
+        favCharactersId.self = getAllCharactersIdFromLocalDatabaseUseCase.execute()
+    }
+    
+    private func insertFavCharacterIdIntoLocalDatabase(_ favCharacterId: Int) {
+        insertFavCharacterIdIntoLocalDatabaseUseCase.execute(favCharacterId: FavCharacterId(favCharactersId: favCharacterId))
+    }
+    
+    private func removeFavCharacterIsFromLocalDatabase(_ favCharacterId: Int) {
+        removeFavCharacterFromLocalDatabaseUseCase.execute(favCharacterId: favCharacterId)
+    }
+    
+    private func populateFavCharacters() {
+        if !favCharactersId.isEmpty {
+            favCharactersId.forEach { id in
+                if let character = characters.first(where: { $0.id == id }) {
+                    addToFavList(character)
+                }
+            }
+        }
+    }
+    
+    func isFavourite(_ id: Int) -> Bool {
+        return favCharactersId.contains { $0 == id }
     }
 }
